b214f557e62efd64fe5c08af8c8681b012ca4cc7
Shivam Kajaria <shivam.kajaria@rutgers.edu>
2023-02-21 17:59:20 -0500

    html parser basic functionality and able to print DOM

--- src/main.c
+++ src/main.c
@@ -66,6 +66,44 @@ char *read_file(int fd)
     return file_text;
 }
 
+// TODO replace this with a hashmap implementation (NO DEPENDENCIES)
+element_type str2type(char *tag_text)
+{
+    if (strcmp(tag_text, "p") == 0)
+        return P;
+    else if (strcmp(tag_text, "div") == 0)
+        return DIV;
+    else if (strcmp(tag_text, "html") == 0)
+        return HTML;
+    else if (strcmp(tag_text, "head") == 0)
+        return HEAD;
+    else if (strcmp(tag_text, "title") == 0)
+        return TITLE;
+    else if (strcmp(tag_text, "body") == 0)
+        return BODY;
+    else
+        return nulltype;
+}
+
+char *type2str (element_type tag_type)
+{
+    if (tag_type == P)
+        return "p";
+    else if (tag_type == DIV)
+        return "div";
+    else if (tag_type == HEAD)
+        return "head";
+    else if (tag_type == BODY)
+        return "body";
+    else if (tag_type == HTML)
+        return "html";
+    else if (tag_type == TITLE)
+        return "title";
+    else if(tag_type == ROOT)
+        return "root";
+    return "unkown";
+}
+
 element_type get_tag(char *html_text)
 {
     int tag_len;
@@ -76,22 +114,7 @@ element_type get_tag(char *html_text)
     memmove(tag_text, html_text, tag_len);
     tag_text[tag_len] = 0;
 
-    element_type tag_type;
-
-    if (strcmp(tag_text, "p") == 0)
-        tag_type = P;
-    else if (strcmp(tag_text, "div") == 0)
-        tag_type = DIV;
-    else if (strcmp(tag_text, "html") == 0)
-        tag_type = HTML;
-    else if (strcmp(tag_text, "head") == 0)
-        tag_type = HEAD;
-    else if (strcmp(tag_text, "title") == 0)
-        tag_type = TITLE;
-    else if (strcmp(tag_text, "body") == 0)
-        tag_type = BODY;
-    else
-        tag_type = nulltype;
+    element_type tag_type = str2type(tag_text);
 
     free(tag_text);
     return tag_type;
@@ -104,6 +127,7 @@ element *parse_html(char *html_text)
     root->elements = NULL;
     root->num_elements = 0;
     root->parent = NULL;
+    root->text = NULL;
 
     int text_size = strlen(html_text);
 
@@ -122,14 +146,16 @@ element *parse_html(char *html_text)
                 break;
 
             // go through and get the tag now. to be added to parent. switch this to be parent until it is knocked off.
-            parent->num_elements = parent->num_elements + 1;
+            parent->num_elements++;
             parent->elements = realloc(parent->elements, sizeof(element *) * parent->num_elements);
 
+
             element *new_element = malloc(sizeof(element));
-            root->type = get_tag(html_text + i + 1);
-            root->elements = NULL;
-            root->num_elements = 0;
-            root->parent = parent;
+            new_element->type = get_tag(html_text + i + 1);
+            new_element->elements = NULL;
+            new_element->num_elements = 0;
+            new_element->parent = parent;
+            new_element->text = NULL;
 
             parent->elements[parent->num_elements - 1] = new_element;
 
@@ -153,12 +179,30 @@ void free_DOM(element *root)
 {
     if (root->num_elements == 0)
     {
-        free(root->text);
+        if (root->text != NULL)
+            free(root->text);
         free(root);
         return;
     }
     for (int i = 0; i < root->num_elements; i++)
+    {
         free_DOM(root->elements[i]);
+    }
+    free(root->elements);
+    if (root->text != NULL)
+        free(root->text);
+
+    free(root);
+}
+
+void print_DOM(element *root, int level)
+{
+    for (int i = 0; i < level; i++)
+        printf("|");
+    printf("%s\n", type2str(root->type));
+
+    for (int j = 0; j < root->num_elements; j++)
+        print_DOM(root->elements[j], level+1);
 }
 
 int main(int argc, char **argv)
@@ -188,6 +232,9 @@ int main(int argc, char **argv)
 
     element *root = parse_html(html_text);
 
+    print_DOM(root, 0);
+
     free(html_text);
+    free_DOM(root);
     return EXIT_SUCCESS;
 }
